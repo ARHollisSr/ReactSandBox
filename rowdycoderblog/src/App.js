5e6379d9f3b4fe09798af950ee7510c1d5977472
FocusedBit <anthonyrayhollis@gmail.com>
2019-01-17 19:58:48 -0500

    Update App.js

--- rowdycoderblog/src/App.js
+++ rowdycoderblog/src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    var helloWorld = "Welcome to the Road to learn React";
+    var user = {
+      firstName: "Anthony",
+      lastName: "Hollis"
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <h2>{helloWorld}</h2>
+       <p>{user.firstName}</p>
+       <p>{user.lastName}</p>
       </div>
     );
   }
