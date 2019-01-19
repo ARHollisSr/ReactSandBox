3e175ce14f63b697a290e2e3617e3bd6df97b7e8
FocusedBit <anthonyrayhollis@gmail.com>
2019-01-18 23:05:29 -0500

    Update index.js

--- rowdycoderblog/src/index.js
+++ rowdycoderblog/src/index.js
@@ -1,11 +1,10 @@
-import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<h1>Passing JSX - devoid of a component</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
