e0b506fdadf59d4c67286f8dab30b3882bb93ee4
FocusedBit <anthonyrayhollis@gmail.com>
2019-01-19 22:10:44 -0500

    Update App.js
    Added arrow function to make the code more readable. Using an implicit return statement

--- rowdycoderblog/src/App.js
+++ rowdycoderblog/src/App.js
@@ -25,16 +25,14 @@ class App extends Component {
   render() {
     return(
     <div className="App">
-      {list.map(function(item){
-        return (
+      {list.map(item =>         
           <div key = {item.objectID}>
             <span><a href ={item.url}>{item.title}</a> &nbsp;</span>            
             <span>{item.author}</span> &nbsp;
             <span>{item.numComments}</span> &nbsp;
             <span>{item.points}</span>            
-          </div>
-        )
-      })}
+          </div>        
+      )}
     </div>
     );
   }
