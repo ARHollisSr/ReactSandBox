51837c1eca2a03933fafaafeff60c1a1d0e3d1cc
FocusedBit <anthonyrayhollis@gmail.com>
2019-01-19 21:44:59 -0500

    update App.js

--- rowdycoderblog/src/App.js
+++ rowdycoderblog/src/App.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react';
 import './App.css';
+
+import React, {Component} from 'react';
+
+const list = [
+  {
+    title: 'React',
+    url: 'http://reactjs.org',
+    author: 'Jordan Walker',
+    numComments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'http://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    numComments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 class App extends Component {
   render() {
-    const helloWorld = "Welcome to the Road to learn React";
-    var user = {
-      firstName: "Anthony",
-      lastName: "Hollis"
-    };
-    return (
-      <div className="App">
-       <h2>{helloWorld}</h2>
-       <p>{user.firstName}</p>
-       <p>{user.lastName}</p>
-      </div>
+    return(
+    <div className='App'>
+      {list.map(function(item){
+        return <div>{item.title}</div>
+      })}
+    </div>
     );
   }
 }
