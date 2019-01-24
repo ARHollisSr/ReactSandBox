ab3ac3099ff590a0016ca6d34431960066eed7c8
FocusedBit <anthonyrayhollis@gmail.com>
2019-01-23 22:29:23 -0500

    Update App.js

--- rowdycoderblog/src/App.js
+++ rowdycoderblog/src/App.js
@@ -64,64 +64,48 @@ class App extends Component {
   }
 }
 
-class Button extends Component {
-  render() {
-    const { onClick, className = '', children, } = this.props;
-    return (
-      <button
-        onClick={onClick}
-        className={className}
-        type="button"
-      >
-        {children}
-      </button>
-    );
-  }
-}
+const Button = ({ onClick, className = '', children }) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children} <input
-          typer="text"
-          value={value}
-          onChange={onChange}
-        />
-      </form>
-    );
-  }
-}
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item => {
-          const onHandleDismiss = () =>
-            this.onDismiss(item.objectID);
-          return (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a> &nbsp;
-            </span>
-              <span>{item.author}</span> &nbsp;
-            <span>{item.numComments}</span> &nbsp;
-            <span>{item.points}</span> &nbsp;
-            <span>
-              <Button onClick={()=>onDismiss(item.objectID)}>
+const Search = ({value, onChange, children}) =>
+    <form>
+      {children} <input
+        typer="text"
+        value={value}
+        onChange={onChange}
+      />
+    </form>
+
+const Table = ({ list, pattern, onDismiss }) =>
+  <div>
+    {list.filter(isSearched(pattern)).map(item => {
+      const onHandleDismiss = () =>
+        onDismiss(item.objectID);
+      return (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a> &nbsp;
+          </span>
+          <span>{item.author}</span> &nbsp;
+          <span>{item.numComments}</span> &nbsp;
+          <span>{item.points}</span> &nbsp;
+          <span>
+            <Button onClick={() => onDismiss(item.objectID)}>
               Dismiss
-              </Button>   
-            </span>
-            </div>
-          );
-        }
-        )}
-      </div>
-    );
-  }
-}
+            </Button>
+          </span>
+        </div>
+        );
+       }
+      )
+    }
+  </div>
 
 export default App;
