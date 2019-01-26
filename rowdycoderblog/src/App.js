3e1c2ae02dc79314ece7374ca6fb166cbcc40586
FocusedBit <anthonyrayhollis@gmail.com>
2019-01-26 08:17:05 -0500

    Update App.js
    Added functionality to access Hacker News API

--- rowdycoderblog/src/App.js
+++ rowdycoderblog/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import React, {Component} from 'react';
 
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'http://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
 const list = [
   {
     title: 'React',
@@ -27,11 +32,25 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: list,
-      searchTerm: '',
+      result:null,
+      searchTerm:DEFAULT_QUERY,
     };
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({result});
+  }
+
+  componentDidMount() {
+    const {searchTerm} = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    .then(response=>response.json())
+    .then(result=>this.setSearchTopStories(result))
+    .catch(error=>error);
   }
 
   onSearchChange(event) { //event has the value of the input field in its target object
@@ -45,7 +64,9 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+    console.log(this.state);
+    if(!result) {return null;}
     return (
       <div className="page">
       <div className="interactions">
@@ -57,7 +78,7 @@ class App extends Component {
         </Search>
         </div>
         <Table
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
