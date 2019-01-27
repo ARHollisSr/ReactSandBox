f4a142bf665611bec27c17757ab39ca177ff665e
FocusedBit <anthonyrayhollis@gmail.com>
2019-01-27 14:10:09 -0500

    Update App.js

--- rowdycoderblog/src/App.js
+++ rowdycoderblog/src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 import React, {Component} from 'react';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
 const PATH_BASE = 'http://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 const list = [
   {
@@ -45,8 +48,8 @@ class App extends Component {
     this.onDismiss = this.onDismiss.bind(this);
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page=0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
     .then(response=>response.json())
     .then(result=>this.setSearchTopStories(result))
     .catch(error=>error);
@@ -59,7 +62,10 @@ class App extends Component {
   }
 
   setSearchTopStories(result) {
-    this.setState({result});
+    const { hits, page } = result;
+    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const updatedHits = [...oldHits, ...hits];
+    this.setState({ result: { hits: updatedHits, page } });
   }
 
   componentDidMount() {
@@ -85,6 +91,7 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
+    const page = (result && result.page) || 0;
     console.log(this.state);
     if(!result) {return null;}
     return (
@@ -106,6 +113,12 @@ class App extends Component {
         />
         : null
         }
+        <div className="interactions">
+          <Button onClick={()=>
+          this.fetchSearchTopStories(searchTerm, page + 1)}>
+          More
+          </Button>
+        </div>
       </div>
     );
   }
