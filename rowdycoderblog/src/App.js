78e4e3ad367aac7fea33314a7016f17cd3bf128c
FocusedBit <anthonyrayhollis@gmail.com>
2019-01-29 20:53:33 -0500

    Update App.js

--- rowdycoderblog/src/App.js
+++ rowdycoderblog/src/App.js
@@ -42,11 +42,16 @@ class App extends Component {
       searchTerm:DEFAULT_QUERY,
     };
     console.log("RESULTS: " + this.state.results);
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  needsToSearchTopStories(searchTerm) {
+    return [this.state.results[searchTerm]];
   }
 
   fetchSearchTopStories(searchTerm, page=0) {
@@ -57,9 +62,11 @@ class App extends Component {
   }
 
   onSearchSubmit(event) {
-    const {searchTerm} = this.state;
-    this.setState({searchKey:searchTerm});
-    this.fetchSearchTopStories(searchTerm);
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
     event.preventDefault();
   }
 
